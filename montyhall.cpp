e2e7c19fab99a4d95a21852df3d51d93b3b84e38
H-sooyeon <wndqhr__@naver.com>
2023-05-01 20:30:32 +0900

    feat: Keep User Selection
    랜덤으로 스포츠카 넣고 바꾸지 않을 경우의 결과

--- montyhall.cpp
+++ montyhall.cpp
@@ -3,22 +3,30 @@
 #include<algorithm>
 #include<vector>
 #include<ctime>
- 
 using namespace std;
- 
 int main()
-{    int count = 0;
-    srand((unsigned int)time(NULL));   
-    for(int n = 0; n < 10000; n ++)
+{
+    int count = 0;
+    srand((unsigned int)time(NULL));
+    for (int n = 0; n < 10000; n++)
     {
         vector<int> choices;
         choices.push_back(0);
         choices.push_back(0);
         choices.push_back(1);
-        
         random_shuffle(choices.begin(), choices.end());
-    
+        int choice = rand() % 3;
+        for (int i = 0; i < 3; i++) {
+            if (choices[i] == 0 && choice != i)
+            {
+                choices.erase(choices.begin() + i);
+                break;
+            }
+        }
+        choice = 1 - min(choice, 1);
+        count += choices[choice];
+        choices.clear();
     }
-    // cout << count << endl;
+    cout << count << endl;
     return 0;
 }
